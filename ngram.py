b2ba6e40f943b31f7675cc578d761a0324419cf2
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-12 17:16:02 +0200

    Non tested code computing tf_idf

--- ngram.py
+++ ngram.py
@@ -2,6 +2,6 @@ class Ngram:
     def __init__(self, mot, doc):
         self.mot = mot
         self.occu_tot = 1
-        self.docs = []
-        self.docs.append(doc)
+        self.docs = set()
+        self.docs.add(doc)
         self.tf_idf = 0.0
--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from io import StringIO
 import re
 import sys
 from ngram import Ngram
+import numpy as np
 
 # taken from stackoverflow.com/questions/26494211/extracting-text-from-file-using-pdfminer-in-python
 def convert_pdf_to_txt(path):
@@ -41,29 +42,52 @@ A n-gram is a group of n words.
 :return: the ngrams
 :typer: list
 """
-def make_ngram(splited, n, n_doc):
+def make_ngram(ngrams, splited, n, n_doc):
     tmp = []
-    ngrams = []
-    len_splited = int((len(splited) / n) + 0.5)
-    print(len_splited)
-    for i in range(0, len_splited):
+    for i in range(0, len(splited)):
         for j in range(0, n):
-            if i * n + j < len( splited):
-                tmp.append(splited[i * n + j])
-        ngram = Ngram(tmp, n_doc)
-        ngrams.append(ngram)
+            if i + j < len(splited):
+                tmp.append(splited[i + j])
+        if len(tmp) == n:
+            key = ' '.join(tmp)
+            if key in ngrams:
+                ngrams[key].occu_tot
+            ngram = Ngram(tmp, n_doc)
+            ngrams[key] = ngram
         tmp = []
     return ngrams
+
+def get_max_occu(ngrams):
+    res = None
+    for k, i in ngrams.items():
+        if res == None:
+            res = i
+            continue
+        if res.occu_tot > i.occu_tot:
+            res = i
+    return res.occu_tot
+
+def compute_tfidf(ngrams, nb_doc):
+    max_occu = get_max_occu(ngrams)
+    for k, item in ngrams:
+        tf = item.occu_tot / max_occu
+        idf = np.log(nb_doc / len(item.docs))
+        item.tf_idf = tf * idf
 
 def main(argv):
     text = convert_pdf_to_txt("doc.pdf")
     splited = re.findall(r"[\w']+", text)
-    ngrams = make_ngram(splited, 4, 1)
-    for i in range(len(ngrams)):
-        print(ngrams[i].mot)
+    ngrams = {}
+    ngrams = make_ngram(ngrams, splited, 2, 1)
+    for key, item in ngrams.items():
+        print(key)
 
 
 if __name__ == "__main__":
     main(sys.argv[1:])
 
-
+#ngram = Ngram(['toto'], 1)
+#dico = {}
+#dico[''.join(ngram.mot)] = ngram
+#for key, item in dico.items():
+#    print(key + " : ", item)
