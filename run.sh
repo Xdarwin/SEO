b3246ab73c127cb517eb90107cda0404050df913
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-13 21:00:28 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo 'Installing pdfminer library'
+pip install pdfminer.six
 
 echo 'Processing the files'
 echo 'Command line tool:'
 echo 'write [word] : get suggestions about [word]'
 echo 'quit : quit the program'
+echo 'Waiting for the proccessing of files to finish'
 python run.py ricou/internet_chap1.pdf ricou/internet_chap2.pdf -n 2 -r 5
