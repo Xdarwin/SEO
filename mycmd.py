fde63a16f79e91054aaa161fc80714ce9b77d32e
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2018-07-13 22:22:48 +0200

    Remove the <TAB>, replace it by <ENTER>

--- mycmd.py
+++ mycmd.py
@@ -17,9 +17,7 @@ class myCmd(cmd.Cmd):
         return True
 
     def do_write(self, s):
-        pass
-
-    def complete_write(self, text, line, begidx, endidx):
+        text = s
         if not text:
             res = self.ngrams
         else:
@@ -33,8 +31,25 @@ class myCmd(cmd.Cmd):
             for i in range(min_res):
                 tmp = ' '.join(res[i].mot)
                 print(tmp + " => score tf_idf: " + str(res[i].tf_idf))
-        print(text, end='', flush=True)
-        return res 
+#        return res 
+
+
+#    def complete_write(self, text, line, begidx, endidx):
+#        if not text:
+#            res = self.ngrams
+#        else:
+#            res = []
+#            for i in range(len(self.ngrams)):
+#                mot = ' '.join(self.ngrams[i].mot)
+#                if mot.startswith(text):
+#                    res.append(self.ngrams[i])
+#            min_res = min(len(res), self.result)
+#            print("\nThis is the best ngrams we can find:")
+#            for i in range(min_res):
+#                tmp = ' '.join(res[i].mot)
+#                print(tmp + " => score tf_idf: " + str(res[i].tf_idf))
+#        print(text, end='', flush=True)
+#        return res 
 
     """
     Print the nb first ngrams.
